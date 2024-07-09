2479afd0579aae6c2231982684a875b7ef2d1f85
Reymond Bolambao <reymond.bolambao@churchillre.com>
2024-07-09 19:10:08 +0800

    adjust sign up layout

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -7,21 +7,21 @@
                 <h1 class="h3 mb-3 fw-normal text-center">Create Account</h1>
 
                 <div class="form-floating mb-2">
-                    <input type="email" class="form-control border-input-important border-bottom" id="floatingInput" placeholder="Username">
+                    <input type="email" class="form-control border-top-0 border-start-0 border-end-0" id="floatingInput" placeholder="Username">
                     <label for="floatingInput">Username</label>
                 </div>
                 <div class="form-floating mb-2">
-                    <input type="password" class="form-control border-input-important border-bottom" id="floatingPassword" placeholder="Password">
+                    <input type="password" class="form-control border-top-0 border-start-0 border-end-0" id="floatingPassword" placeholder="Password">
                     <label for="floatingPassword">Password</label>
                 </div>
 
                 <div class="form-floating mb-2">
-                    <input type="firstname" class="form-control border-input-important border-bottom" id="floatingInput" placeholder="First Name">
+                    <input type="firstname" class="form-control border-top-0 border-start-0 border-end-0" id="floatingInput" placeholder="First Name">
                     <label for="floatingInput">First Name</label>
                 </div>
 
                 <div class="form-floating mb-3">
-                    <input type="lastname" class="form-control border-input-important border-bottom" id="floatingInput" placeholder="Last Name">
+                    <input type="lastname" class="form-control border-top-0 border-start-0 border-end-0" id="floatingInput" placeholder="Last Name">
                     <label for="floatingInput">Last Name</label>
                 </div>
 

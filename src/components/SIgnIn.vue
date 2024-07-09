990122024197652cdaa0975e811815d9151bb7ee
Reymond Bolambao <reymond.bolambao@churchillre.com>
2024-07-09 19:02:03 +0800

    Adjust sign in and up layout

--- src/components/SIgnIn.vue
+++ src/components/SIgnIn.vue
@@ -7,11 +7,11 @@
                 <h1 class="h3 mb-3 fw-normal text-center">Sign In</h1>
 
                 <div class="form-floating mb-2">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="Username">
+                    <input type="email" class="form-control border-input-important border-bottom" id="floatingInput" placeholder="Username">
                     <label for="floatingInput">Username</label>
                 </div>
                 <div class="form-floating mb-3">
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    <input type="password" class="form-control border-input-important border-bottom" id="floatingPassword" placeholder="Password">
                     <label for="floatingPassword">Password</label>
                 </div>
 
